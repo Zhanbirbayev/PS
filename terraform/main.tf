f1874b0e7a8c6d2d4af3cf328059a9aacf43b45f
Zhanbirbayev <90315769+Zhanbirbayev@users.noreply.github.com>
2026-03-31 20:23:23 +0500

    Update to add main.tf
    Added variable for private key path and updated security group rules to allow traffic on port 8080. Included Ansible execution resource for provisioning.

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,10 @@ variable "image_id" {
   default = "3f5b3bae-e421-4d1f-9f1c-45e363187b11"
 }
 
-# Описание 5 серверов
+variable "private_key_path" {
+  default = "C:/Users/Nurzhan.Zhanbirbayev/Documents/PS/pscloud_key"
+}
+
 variable "vms" {
   default = {
     "haproxy" = "192.168.0.10"
@@ -44,7 +47,7 @@ resource "openstack_networking_subnet_v2" "private_subnet" {
   name       = "task-subnet"
   network_id = openstack_networking_network_v2.private_network.id
   cidr       = "192.168.0.0/24"
-  dns_nameservers = ["195.210.46.195", "195.210.46.132"]
+  dns_nameservers = ["8.8.8.8"]
 }
 
 resource "openstack_networking_router_v2" "router" {
@@ -58,15 +61,16 @@ resource "openstack_networking_router_interface_v2" "router_interface" {
 }
 
 ##############################
-# Security Groups (SSH + HTTP + ICMP)
+# Security Groups (SSH + HTTP + Stats + ICMP)
 ##############################
 resource "openstack_networking_secgroup_v2" "sg" {
   name        = "task-sg"
-  description = "Allow SSH, HTTP, ICMP"
+  description = "Allow SSH, HTTP, Stats, ICMP"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "rules" {
-  for_each          = toset(["22", "80"])
+  # Добавляем 8080 для мониторинга HAProxy
+  for_each          = toset(["22", "80", "8080"])
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
@@ -92,7 +96,6 @@ resource "openstack_compute_keypair_v2" "ssh" {
   public_key = file("C:/Users/Nurzhan.Zhanbirbayev/Documents/PS/pscloud_key.pub")
 }
 
-# Создаем 5 дисков
 resource "openstack_blockstorage_volume_v3" "volumes" {
   for_each    = var.vms
   name        = "vol-${each.key}"
@@ -101,7 +104,6 @@ resource "openstack_blockstorage_volume_v3" "volumes" {
   volume_type = "ceph-ssd"
 }
 
-# Создаем 5 портов с фиксированными IP
 resource "openstack_networking_port_v2" "ports" {
   for_each       = var.vms
   name           = "port-${each.key}"
@@ -113,7 +115,6 @@ resource "openstack_networking_port_v2" "ports" {
   }
 }
 
-# Создаем 5 инстансов
 resource "openstack_compute_instance_v2" "instances" {
   for_each    = var.vms
   name        = each.key
@@ -132,12 +133,11 @@ resource "openstack_compute_instance_v2" "instances" {
     delete_on_termination = true
   }
 
-  # Скрипт только для Control Node: установка Ansible
   user_data = each.key == "control" ? "#!/bin/bash\nyum install -y epel-release\nyum install -y ansible git" : null
 }
 
 ##############################
-# Floating IPs (Для HAProxy и Control)
+# Floating IPs
 ##############################
 resource "openstack_networking_floatingip_v2" "fips" {
   for_each = toset(["haproxy", "control"])
@@ -150,10 +150,74 @@ resource "openstack_networking_floatingip_associate_v2" "fip_assoc" {
   port_id     = openstack_networking_port_v2.ports[each.key].id
 }
 
+##############################
+# Ansible Execution
+##############################
+locals {
+  control_public_ip = openstack_networking_floatingip_v2.fips["control"].address
+}
+
+resource "terraform_data" "run_ansible" {
+  depends_on = [
+    openstack_compute_instance_v2.instances,
+    openstack_networking_floatingip_associate_v2.fip_assoc
+  ]
+
+  triggers_replace = {
+    control_ip = local.control_public_ip
+    # Хешируем файлы: если они изменятся на ПК, Terraform перезапустит Ansible
+    hosts_hash = filemd5("${path.module}/ansible/hosts.ini")
+    site_hash  = filemd5("${path.module}/ansible/site.yml")
+  }
+
+  connection {
+    type        = "ssh"
+    host        = local.control_public_ip
+    user        = "centos"
+    private_key = file(var.private_key_path)
+    timeout     = "5m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/centos/ansible",
+      "chmod 700 /home/centos"
+    ]
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/ansible/hosts.ini"
+    destination = "/home/centos/ansible/hosts.ini"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/ansible/site.yml"
+    destination = "/home/centos/ansible/site.yml"
+  }
+
+  provisioner "file" {
+    source      = var.private_key_path
+    destination = "/home/centos/id_rsa"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 600 /home/centos/id_rsa",
+      "sudo yum install -y epel-release || true",
+      "sudo yum install -y ansible git curl tar || true",
+      "cd /home/centos/ansible",
+      "ansible-playbook -i hosts.ini site.yml"
+    ]
+  }
+}
+
+##############################
+# Outputs
+##############################
 output "haproxy_public_ip" {
   value = openstack_networking_floatingip_v2.fips["haproxy"].address
 }
 
 output "control_public_ip" {
-  value = openstack_networking_floatingip_v2.fips["control"].address
+  value = local.control_public_ip
 }
